c798d795e73c7ce3e1275d66f71196099f8bda48
Kewal Kishan Gokuldas <Kewalkishang@gmail.com>
2024-07-15 00:16:04 -0700

    Draft scene for BLE devices to connect and transfer data

--- Assets/Scripts/AndroidPluginTest.cs
+++ Assets/Scripts/AndroidPluginTest.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Android;
+using TMPro;
+using System;
 
 public class AndroidPluginTest : MonoBehaviour
 {
     // Start is called before the first frame update
-     private AndroidJavaObject bluetoothManager;
+    public  TMP_Text DebugText;
+
+     public GameObject deviceButtonPrefab;
+
+     public GameObject scrollContent;
+     private AndroidJavaObject bluetoothPluginManager;
 
     void Start()
     {
@@ -29,19 +37,24 @@ public class AndroidPluginTest : MonoBehaviour
         // }
     }
 
+    void setDebugMsg(string msg){
+         DebugText.text = msg;
+    }
+
      public void testPluginLoad()
     {
         Debug.Log("Testing Plugin Load:");
         try
         {
-            using (AndroidJavaClass pluginClass = new AndroidJavaClass("com.onlykk.androidbluetoothplugin.BluetoothManager"))
+            using (AndroidJavaClass pluginClass = new AndroidJavaClass("com.onlykk.androidbluetoothplugin.BluetoothPluginManager"))
             {
                 Debug.Log("AndroidJavaClass created...");
 
                 if (pluginClass != null)
                 {
                     string message = pluginClass.CallStatic<string>("checkPluginLoad");
-                    Debug.Log("Message received: " + message);
+                    Debug.Log("Plugin Loaded: " + message);
+                    setDebugMsg("Plugin Loaded: " + message);
                 }
                 else
                 {
@@ -64,14 +77,15 @@ public class AndroidPluginTest : MonoBehaviour
          Debug.Log("Testing Bluetooth Support:");
         try
         {
-             using (AndroidJavaClass pluginClass = new AndroidJavaClass("com.onlykk.androidbluetoothplugin.BluetoothManager"))
+             using (AndroidJavaClass pluginClass = new AndroidJavaClass("com.onlykk.androidbluetoothplugin.BluetoothPluginManager"))
             {
                 // Get the instance of the BluetoothManager
-                bluetoothManager = pluginClass.CallStatic<AndroidJavaObject>("getInstance");
+                bluetoothPluginManager = pluginClass.CallStatic<AndroidJavaObject>("getInstance");
             }
-            bluetoothManager.Call("initBluetoothAdapter", GetUnityActivity());
-            bool initResult = bluetoothManager.Call<bool>("isBluetoothSupported");
+            bluetoothPluginManager.Call("initBluetoothPlugin", GetUnityActivity());
+            bool initResult = bluetoothPluginManager.Call<bool>("isBluetoothSupported");
             Debug.Log("Bluetooth Adapter Initialized: " + initResult);
+            setDebugMsg("Bluetooth Adapter Initialized: " + initResult);
 
         }
         catch (AndroidJavaException ex)
@@ -89,9 +103,9 @@ public class AndroidPluginTest : MonoBehaviour
          Debug.Log("Testing is Bluetooth Enabled:");
         try
         {
-            bool initResult = bluetoothManager.Call<bool>("isBluetoothEnabled");
+            bool initResult = bluetoothPluginManager.Call<bool>("isBluetoothEnabled");
             Debug.Log("Is Bluetooth Enabled : " + initResult);
-
+                  setDebugMsg("Is Bluetooth Enabled : " + initResult);
         }
         catch (AndroidJavaException ex)
         {
@@ -135,17 +149,29 @@ public class AndroidPluginTest : MonoBehaviour
             Debug.Log("android.permission.BLUETOOTH_SCAN permission has been granted.");
 
         }
-            
+
+         if (!Permission.HasUserAuthorizedPermission("android.permission.BLUETOOTH_CONNECT"))
+        {
+             Permission.RequestUserPermissions(new string[] {
+    "android.permission.BLUETOOTH_CONNECT"
+  });
+        }else{
+            Debug.Log("android.permission.BLUETOOTH_CONNECT permission has been granted.");
+
+        }
+    
+             setDebugMsg("All permission permitted! " );
     }
 
 
-     public void testinitializeReceiver()
+     public void teststartDiscovery()
     {
          Debug.Log("Testing is initializeReceiver :");
         try
         {
-            bluetoothManager.Call("initializeReceiver");
-            Debug.Log("initializeReceiver : ");
+            bool startDiscovery = bluetoothPluginManager.Call<bool>("startDiscoveredDevices");
+            Debug.Log("startDiscovery : " + startDiscovery);
+            setDebugMsg("startDiscovery : " + startDiscovery);
 
         }
         catch (AndroidJavaException ex)
@@ -159,17 +185,17 @@ public class AndroidPluginTest : MonoBehaviour
     }
 
 
-    private IEnumerator CheckDiscoveredDevices()
-    {
-        yield return new WaitForSeconds(10); // Wait for 10 seconds to discover devices
+    // private IEnumerator CheckDiscoveredDevices()
+    // {
+    //     yield return new WaitForSeconds(10); // Wait for 10 seconds to discover devices
 
-        string[] discoveredDevices = bluetoothManager.Call<string[]>("getDiscoveredDevices");
+    //     string[] discoveredDevices = bluetoothPluginManager.Call<string[]>("getDiscoveredDeviceDetails");
 
-        foreach (string device in discoveredDevices)
-        {
-            Debug.Log("Discovered Device: " + device);
-        }
-    }
+    //     foreach (string device in discoveredDevices)
+    //     {
+    //         Debug.Log("Discovered Device: " + device);
+    //     }
+    // }
 
 
      public void testgetDiscoveredDevices()
@@ -181,11 +207,24 @@ public class AndroidPluginTest : MonoBehaviour
             //string combinedString = string.Join( ",", devices.ToArray() );
             //Debug.Log("getDiscoveredDevices : " + combinedString );
             //StartCoroutine(CheckDiscoveredDevices());
-            string[] discoveredDevices = bluetoothManager.Call<string[]>("getDiscoveredDevices");
+            string[] discoveredDevices = bluetoothPluginManager.Call<string[]>("getDiscoveredDeviceDetails");
  Debug.Log("Discovered Device: " + discoveredDevices.Length);
+  setDebugMsg("Discovered Device:  " + discoveredDevices.Length);
         foreach (string device in discoveredDevices)
         {
             Debug.Log("Discovered Device: " + device);
+            GameObject deviceButton = Instantiate(deviceButtonPrefab, scrollContent.transform);
+            deviceButton.SetActive(true); // Enable the text object
+            // Set the text of the button
+              TMP_Text buttonText = deviceButton.GetComponentInChildren<TMP_Text>();
+            if (buttonText != null)
+            {
+                buttonText.text = device;
+            }
+
+
+            // Optionally, you can add a listener to the button if you want to handle button clicks
+            deviceButton.GetComponent<Button>().onClick.AddListener(() => OnDeviceButtonClick(device));
         }
         }
         catch (AndroidJavaException ex)
@@ -198,13 +237,195 @@ public class AndroidPluginTest : MonoBehaviour
         }
     }
 
+     public void testStartGATTServer()
+    {
+         Debug.Log("Testing StartGATT Server:");
+        try
+        {
+            bluetoothPluginManager.Call("startGattServer");
+            Debug.Log("StartGATT Server Started : ");
+              setDebugMsg("StartGATT Server Started : Server");
+        }
+        catch (AndroidJavaException ex)
+        {
+            Debug.LogError("AndroidJavaException caught: " + ex.Message);
+        }
+        catch (System.Exception ex)
+        {
+            Debug.LogError("Exception caught: " + ex.Message);
+        }
+    }
+
+     public void testBLEScan()
+    {
+         Debug.Log("Testing Service Scan:");
+        try
+        {
+            bluetoothPluginManager.Call("startBLEDiscovery");
+            Debug.Log("StartGATT Service Scan : ");
+             setDebugMsg("StartGATT Service Scan : Client ");
+
+        }
+        catch (AndroidJavaException ex)
+        {
+            Debug.LogError("AndroidJavaException caught: " + ex.Message);
+        }
+        catch (System.Exception ex)
+        {
+            Debug.LogError("Exception caught: " + ex.Message);
+        }
+    }
+string connectedDevice = "";
+
+    public void OnDeviceButtonClick(string deviceInfo)
+{
+    setDebugMsg("Trying to connect to : " + deviceInfo);
+      Debug.Log("Trying to connect to : " + deviceInfo);
+    // You can expand this to connect to the device or perform other actions
+
+     string[] parts = deviceInfo.Split(new string[] { " - " }, StringSplitOptions.None);
+    if (parts.Length > 1)
+    {
+        string deviceAddress = parts[1];
+
+        // Proceed with the connection using the extracted address
+        try
+        {
+            bluetoothPluginManager.Call("connectToServer", deviceAddress);
+            connectedDevice = deviceAddress;
+            Debug.Log("Connecting to server: " + deviceAddress);
+            setDebugMsg("Connecting to server: " + deviceAddress);
+        }
+        catch (AndroidJavaException ex)
+        {
+            Debug.LogError("AndroidJavaException caught: " + ex.Message);
+        }
+        catch (System.Exception ex)
+        {
+            Debug.LogError("Exception caught: " + ex.Message);
+        }
+    }
+    else
+    {
+        Debug.LogError("Failed to extract device address from info: " + deviceInfo);
+        setDebugMsg("Failed to extract device address from info: " + deviceInfo);
+    }
+
+}
+
+  public void testmsgFromServer()
+{
+      setDebugMsg("Trying testmsgFromServer : ");
+      Debug.Log("Trying testmsgFromServer : ");
+    // You can expand this to connect to the device or perform other actions
+
+  
+        // Proceed with the connection using the extracted address
+        try
+        {
+            string msg = bluetoothPluginManager.Call<string>("getMsgFromServer");
+            Debug.Log("testmsgFromServer : " + msg);
+            setDebugMsg("testmsgFromServer : " + msg);
+        }
+        catch (AndroidJavaException ex)
+        {
+            Debug.LogError("AndroidJavaException caught: " + ex.Message);
+        }
+        catch (System.Exception ex)
+        {
+            Debug.LogError("Exception caught: " + ex.Message);
+        }
+       
+}
+
+  public void testmsgFromClient()
+{
+      setDebugMsg("Trying testmsgFromClient : ");
+      Debug.Log("Trying testmsgFromClient : ");
+    // You can expand this to connect to the device or perform other actions
+
+
+        // Proceed with the connection using the extracted address
+        try
+        {
+             string msg = bluetoothPluginManager.Call<string>("getMsgFromClient");
+            Debug.Log("testmsgFromClient : " + msg);
+            setDebugMsg("testmsgFromClient : " + msg);
+        }
+        catch (AndroidJavaException ex)
+        {
+            Debug.LogError("AndroidJavaException caught: " + ex.Message);
+        }
+        catch (System.Exception ex)
+        {
+            Debug.LogError("Exception caught: " + ex.Message);
+        }
+     
+}
+
+
+  public void testSendToServer()
+{
+      setDebugMsg("Trying to send data to : " + connectedDevice);
+      Debug.Log("Trying to connect to : " + connectedDevice);
+    // You can expand this to connect to the device or perform other actions
+
+        if(connectedDevice != "")
+        {
+        // Proceed with the connection using the extracted address
+        try
+        {
+            bluetoothPluginManager.Call("sendMessageToServer", "Hello from client");
+            Debug.Log("send data to : " + connectedDevice);
+            setDebugMsg("send data to : " + connectedDevice);
+        }
+        catch (AndroidJavaException ex)
+        {
+            Debug.LogError("AndroidJavaException caught: " + ex.Message);
+        }
+        catch (System.Exception ex)
+        {
+            Debug.LogError("Exception caught: " + ex.Message);
+        }
+        }
+        else{
+            Debug.Log("No devices connected " );
+            setDebugMsg("No devices connected " );
+        }
+}
+
+
+  public void testSendToClient()
+{
+      setDebugMsg("Trying to testSendToClient: ");
+      Debug.Log("Trying to testSendToClient : ");
+    // You can expand this to connect to the device or perform other actions
+
+        
+        // Proceed with the connection using the extracted address
+        try
+        {
+            bluetoothPluginManager.Call("sendMessageToClient", "Hello from server!");
+            Debug.Log("send data to Client: " );
+            setDebugMsg("send data to Client: ");
+        }
+        catch (AndroidJavaException ex)
+        {
+            Debug.LogError("AndroidJavaException caught: " + ex.Message);
+        }
+        catch (System.Exception ex)
+        {
+            Debug.LogError("Exception caught: " + ex.Message);
+        }
+}
 
 
     private AndroidJavaObject GetUnityActivity()
     {
         using (AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
         {
-            return unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+            var activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+            return activity.Call<AndroidJavaObject>("getApplicationContext");
         }
     }
     
